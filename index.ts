a8ae04a2be1f97ab269f43a5bfa3902705afbba6
Timothy Otieno <otienotimothyomondi@gmail.com>
2023-08-21 17:30:24 +0300

    add routes

--- index.ts
+++ index.ts
@@ -1,94 +1,79 @@
+import express from 'express'
 import { PrismaClient } from '@prisma/client'
 
+const app = express()
 const prisma = new PrismaClient()
 
+app.use(express.json())
 
-/**
- * 1. Create A New User
- */
-const createUser = async () => {
+
+app.get('/users', async (req, res) => {
   try {
-    await prisma.user.create({
+    const users = await prisma.user.findMany()
+    res.json(users)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/signup', async (req, res) => {
+  try {
+    const { name, email } = req.body
+    const newUser = await prisma.user.create({
       data: {
-        name: "Tim",
-        email: "tim@example.com",
-        posts: {
-          create: {
-            title: 'Getting Started With Prisma',
-            slug: 'getting-started-with-prisma',
-            body: 'A have no idea what to add here but you get the gist'
+        name,
+        email
+      }
+    })
+    res.json(newUser)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/posts', async (req, res) => {
+  try {
+    const { title, content, authorEmail } = req.body
+
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        content,
+        author: {
+          connect: {
+            email: authorEmail
           }
         }
       }
     })
+
+    res.json(newPost)
   } catch (error) {
-    console.error(error)
+    console.log(error)
   }
-}
+})
 
-// createUser()
-
-/**
- * 2. Get All Users
- */
-const getAllUsers = async () => {
+app.put("/posts/:id/views", async (req, res) => {
   try {
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
-  } catch (error) {
-    console.error(error)
-  }
-}
+    const { id } = req.params;
 
-// getAllUsers()
-
-
-/**
- * 3. Update Posts
- */
-const updatePost = async () => {
-  try {
-    await prisma.post.update({
+    const updatePost = await prisma.post.update({
       where: {
-        slug: 'getting-started-with-prisma'
+        id,
       },
       data: {
-        comments: {
-          createMany: {
-            data: [
-              {
-                comment: 'This is comment One'
-              },
-              {
-                comment: 'This is another One'
-              }
-            ]
-          }
-        }
-      }
-    })
+        viewCount: {
+          increment: 1,
+        },
+      },
+    });
+
+  res.json(updatePost);
   } catch (error) {
-    
+    console.log(error)
   }
-}
+});
 
-// updatePost()
-
-/**
- * 4. Get All Posts
- */
-
-const getAllPosts = async () => {
-  try {
-    const posts = await prisma.post.findMany({
-      include: {
-        comments: true
-      }
-    })
-    console.log(posts)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-// getAllPosts()
+app.listen(3000, () => {
+  console.log(`Server running on PORT, 3000`)
+})
